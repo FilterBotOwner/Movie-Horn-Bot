e6b00722d3586d8b6fdae5a0756f04092b917073
fileuplaoderbot <tparveen2728@gmail.com>
2024-12-29 01:01:31 +0530

    Update info.py

--- info.py
+++ info.py
@@ -13,8 +13,8 @@ def is_enabled(value, default):
 #---------------------------------------------------------------
 #---------------------------------------------------------------         ,
 SESSION = environ.get('SESSION', 'Media_search')
-API_ID = int(environ.get('API_ID', ''))
-API_HASH = environ.get('API_HASH', '')
+API_ID = int(environ.get('API_ID', '26683574'))
+API_HASH = environ.get('API_HASH', '69ba051f43cff367bf569bd54eb277a7')
 BOT_TOKEN = environ.get('BOT_TOKEN', '')
 #---------------------------------------------------------------
 #---------------------------------------------------------------
